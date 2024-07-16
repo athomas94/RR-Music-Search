5daafc34237c5f63c6ad6eef553c95a6a123aea6
Andrew Thomas <athomas94@gmail.com>
2024-07-16 12:24:48 -0400

    CORS Policy forever

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SearchBar from './components/Searchbar'
